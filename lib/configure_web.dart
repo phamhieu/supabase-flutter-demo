40a490dbea8dc1f3cc331d4403d7d9d40eb6ccf9
phamhieu <phamhieu1998@gmail.com>
2021-09-03 10:38:35 +0700

    chore: update comment

--- lib/configure_web.dart
+++ lib/configure_web.dart
@@ -4,7 +4,7 @@ import 'utils/constants.dart';
 
 void configureApp() {
   // init Supabase singleton
-  // no localStorage provided, fallback to use shared_preferences
+  // no localStorage provided, fallback to use hive as default
   Supabase.initialize(
     url: supabaseUrl,
     anonKey: supabaseAnnonKey,
